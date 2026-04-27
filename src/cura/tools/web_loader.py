3005861e2e6cde745e5578fda3ddffc757499f34
kyu-dev <100522666+kyu-dev@users.noreply.github.com>
2026-04-27 20:46:35 +0200

    Refactor code structure for improved readability and maintainability

---
+++ src/cura/tools/web_loader.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_mistralai import MistralAIEmbeddings
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+
+docs[0][0].page_content.strip()[:1000]
+
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, embedding=MistralAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
